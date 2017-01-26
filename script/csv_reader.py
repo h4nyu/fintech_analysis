3fb8886c95ed0cacc1db917b3dc64305bc93affd
x1nyuan <yao.ntno@gmail.com>
2017-01-27 01:59:49 +0900

    model

--- script/csv_reader.py
+++ script/csv_reader.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-import csv
 
-with open("/home/yao/fintech_tutorial/data/0.csv") as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
-        print(row)
+import pandas as pd
+import numpy as np
+
+
+x_train = np.empty((0, 7))
+y_train = np.empty((0))
+for i in range(3):
+    df = pd.read_csv("~/fintech_tutorial/data/{0}.tsv".format(i), delimiter="\t")
+    x_train = np.append(x_train, np.array(df.ix[0:, 2:]), axis=0)
+    y_train = np.append(y_train, np.array(df.ix[0:, 1]), axis=0)
+
+print y_train
+
+
+
+
+
