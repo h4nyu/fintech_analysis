ca78ee35d5e8bffeac9c105221afbc91b9cf00fe
x1nyuan <yao.ntno@gmail.com>
2017-02-26 02:14:35 +0900

    時系列データの内部取り出し

--- script/collector.py
+++ script/collector.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 import requests
 from pprint import pprint
+import matplotlib.pyplot as plt
 
 
 class Collector(object):
@@ -12,31 +13,48 @@ class Collector(object):
         """TODO: to be defined1. """
         pass
         self.yql_url = 'https://query.yahooapis.com/v1/public/yql?'
+        self.datatable_url = 'store://datatables.org/alltableswithkeys'
 
     def get_histrical_data(self, symbol, start_date, end_date):
+        yql = 'select * from yahoo.finance.historicaldata '\
+            + 'where symbol = "{0}" '.format(symbol)\
+            + 'and startDate = "{0}" '.format(start_date)\
+            + 'and endDate = "{0}"'.format(end_date)
 
         pyload = {
-            'q': 'select * from yahoo.finance.historicaldata where symbol = "{0}" and startDate = "{1}" and endDate = "{2}"'.format(symbol, start_date, end_date),
+            'q': yql,
             'format': 'json',
-            'env': 'store://datatables.org/alltableswithkeys'
+            'env': self.datatable_url
         }
 
         r = requests.get(self.yql_url, params=pyload)
-        return r.json()
+        return r.json()['query']['results']['quote']
 
-    def get_quote(self, symbol, start_date, end_date):
+    def get_quote(self, symbol):
+        yql = 'select * from yahoo.finance.quote '\
+            + 'where symbol = "{0}" '.format(symbol)
 
         pyload = {
-            'q': 'select * from yahoo.finance.quote where symbol = "{0}"',
+            'q': yql,
             'format': 'json',
-            'env': 'store://datatables.org/alltableswithkeys'
-        }.format(symbol, start_date, end_date)
+            'env': self.datatable_url
+        }
 
         r = requests.get(self.yql_url, params=pyload)
 
-        return r.json()
+        return r.json()['query']
 
 
 if __name__ == "__main__":
     c = Collector()
-    c.get_histrical_data('YHOO', '2014-02-11', '2014-02-18')
+    r = c.get_histrical_data('YHOO', '2014-02-11', '2014-02-18')
+    closes = [float(i['Close']) for i in r]
+    opens = [float(i['Open']) for i in r]
+    highs = [float(i['High']) for i in r]
+    lows = [float(i['Low']) for i in r]
+    plt.plot(closes)
+    plt.plot(opens)
+    plt.plot(highs)
+    plt.plot(lows)
+    plt.show()
+
