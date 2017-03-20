adafc8abbda941aeb31116a905ecacc299a1d8f5
mizawamikano <nasawake.am@gmail.com>
2017-03-20 21:45:22 +0900

    手数料を計算に入れるように変更

--- account.py
+++ account.py
@@ -5,13 +5,35 @@ class Account(object):
     def __init__(self, _asset = 1000000):
         self.asset = _asset
     def buy(self, _price, _amount):
-        self.asset -= _price * _amount
+        if _price * _amount <= 100000:
+            comm = 100
+        if _price * _amount <= 200000:
+            comm = 180
+        if _price * _amount <= 300000:
+            comm = 250
+        if _price * _amount <= 400000:
+            comm = 350
+        if _price * _amount <= 500000:
+            comm = 450
+        if _price * _amount <= 1000000:
+            comm = 1000
+        if _price * _amount > 1000000:
+            comm + _price * _amount * 0.001
+        print(comm)
+        self.asset -= _price * _amount + comm
     def sell(self, _price ,_amount):
-        self.asset += _price * _amount
-
-def main():
-    user = Account()
-    print(user.asset)
-
-if __name__ == '__main__':
-    main()
+        if _price * _amount <= 100000:
+            comm = 100
+        if _price * _amount <= 200000:
+            comm = 180
+        if _price * _amount <= 300000:
+            comm = 250
+        if _price * _amount <= 400000:
+            comm = 350
+        if _price * _amount <= 500000:
+            comm = 450
+        if _price * _amount <= 1000000:
+            comm = 1000
+        if _price * _amount > 1000000:
+            comm + _price * _amount * 0.001
+        self.asset += _price * _amount - comm
--- method.py
+++ method.py
@@ -3,11 +3,8 @@
 
 from account import Account
 
-
 def main():
     user = Account()
-    print(user.asset)
-
 
 if __name__ == '__main__':
     main()
