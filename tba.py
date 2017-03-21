d07a4971551a019fbf19253deed4ba96129e4698
x1nyuan <yao.ntno@gmail.com>
2017-03-21 22:06:37 +0900

    fix syntax

--- tba.py
+++ tba.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import numpy as np
 
-class Threat:
+
+class Threat(object):
+
     def __init__(self, _emax=120, _emin=110):
-        emax = _emax
-        emin = _emax
-        #ratio = newton(f, df, 2, 3)
-        #print(ratio)
-        print(emax,emin)
+        self.emax = _emax
+        self.emin = _emin
+        ratio = self.newton(self.f, self.df, 2, 3)
+        print(ratio)
+        print(self.emax, self.emin)
+
     def setdata(self, _max, _min, _current):
         print(100)
-    def f(x):
-        return float(x) - log((emax - emin) / emin) + log(x-1)
-    def df(x):
-        return 1 - emin / (emax-emin) + 1 / (x-1)
-    def newton(f, df, a0, n):
-        if n == 0: return a0
-        a = newton(f, df, a0, n-1)
+
+    def f(self, x):
+        return float(x) - np.log((self.emax - self.emin) / float(self.emin)) + np.log(x - 1)
+
+    def df(self, x):
+        return 1 - self.emin / (self.emax - self.emin) + 1 / (x - 1)
+
+    def newton(self, f, df, a0, n):
+        if n == 0:
+            return a0
+        a = self.newton(f, df, a0, n - 1)
         return a - float(f(a)) / df(a)
 
-def main():
-    usd = Threat(120, 100)
 
 if __name__ == '__main__':
-    main()
+    usd = Threat(120, 100)
