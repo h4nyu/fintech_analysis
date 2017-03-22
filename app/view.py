868d34d9dab442efa7ed965a63ab0a52a34f95ea
x1nyuan <yao.ntno@gmail.com>
2017-03-23 00:07:05 +0900

    jinja2 template data binding

--- app/view.py
+++ app/view.py
@@ -1,7 +1,13 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello fintech'
+    arg0 = 'aaaa'
+    arg1 = {'subarg0': 'bbbb'}
+    return render_template('index.html',
+                           arg0=arg0,
+                           arg1=arg1
+                           )
