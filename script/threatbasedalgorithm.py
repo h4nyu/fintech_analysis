e98265f96e0ded955254fa833913a49c953d6be7
mizawamikano <nasawake.am@gmail.com>
2017-03-22 14:10:28 +0900

    複数回の売買に対応

--- script/threatbasedalgorithm.py
+++ script/threatbasedalgorithm.py
@@ -4,11 +4,11 @@ import numpy as np
 
 class Threat(object):
 
-    def __init__(self, _emax, _emin, _asset):
+    def __init__(self, _emax, _emin, _asset, _buy):
         self.asset = self.init = _asset
         self.emax = _emax
         self.emin = _emin
-        self.vmax = 0
+        self.buy = self.vmax = _buy
         self.vmin = 0
         self.balance = 0
         self.ratio = self.newton(1.15, 5)
@@ -17,14 +17,20 @@ class Threat(object):
         if _current > self.vmax:
             self.vmax = _current
             opt = self.init * self.vmax
-            sell = np.round((opt - self.asset * self.emin * self.ratio)  / self.ratio / (self.vmax - self.emin), 0)
+            sell = np.round((opt - self.asset * self.emin * self.ratio - self.balance * self.ratio)  / self.ratio / (self.vmax - self.emin), 0)
             if sell > 0:
                 self.asset -= sell
                 self.balance += sell * self.vmax
+                # 最適オフラインアルゴリズムの利益
                 opt =  self.vmax * self.init
+                # 現時点での競合比
                 compratio = np.round(opt / (self.balance + self.asset * self.vmax), 3)
+                # 資産
+                total = self.balance + self.asset * self.vmax
+                # 利益
+                benefit = total - self.buy * self.init
                 # print("SELL", sell)
-                print(sell, self.asset, self.balance, compratio)
+                print(sell, self.asset, self.balance, compratio, benefit)
 
     def f(self, x):
         return float(x) - np.log((float(self.emax) / float(self.emin)) - 1) + np.log(x - 1)
@@ -40,5 +46,8 @@ class Threat(object):
 
 # DEBUG STATUS
 if __name__ == '__main__':
-    usd = Threat(120, 100, 10000)
+    usd = Threat(120, 100, 10000, 110)
     usd.input(110)
+    usd.input(111)
+    usd.input(112)
+    usd.input(113)
